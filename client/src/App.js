c6ed72c982f18a965214908022e683e13c357b0c
Li Horne <85302094+lihorne4@users.noreply.github.com>
2022-05-31 21:23:38 -0400

    Corrected Submission
    Sorry Shaun, this should work now. Let me know if you have any questions. 
    
    Cheers!

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Login from './pages/Login';
+import React, { useState, useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+function App() {
+  
+ return <Login /> ;
+    
+}
+
+
+export default App;
---
+++ client/src/usableComponents/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class LoginForm extends Component {
+    render() {
+      return (<> <p> Username</p>
+      <input />
+        <p>Password</p>
+        
+        <input type = "password" />
+
+       </>);
+    }    
+    }
+export default LoginForm
